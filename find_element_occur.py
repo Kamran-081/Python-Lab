7b85365ac817766fe45161af9d53bef87c3a8de8
Kamran Nazir <SheikhKamran1845@gmail.com>
2025-12-04 23:40:11 +0530

    Update find_element_occur.py

--- find_element_occur.py
+++ find_element_occur.py
@@ -1,10 +1,27 @@
 #find all elements occuring more than n/3 times
 
+result = []
 
-elements = []
+nums = [1,1,5,5,5,4,5,2,3,1,1]
+n = len(nums)
+limit = n // 3
 
+for i in range(n):
+    count = 0
 
-nums = [1,1,4,5,5,5,4,2,3,1,1]
-len = len(nums)
-print(len)
+    for j in range(n):
+        if nums[i] == nums[j]:
+            count += 1
 
+    already_added = False
+    for x in result:
+        if nums[i] == x:
+            already_added = True
+            break
+        
+    if count > limit and not already_added:
+        result.append(nums[i])
+    
+    
+print(result)
+    
